6c4b01b38d7050d774899d1a0c7aadb43abd4f80
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-05-25 16:56:17 +0530

    code

---
+++ NG_Validations/Models/ModelValidatorRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NG_Validations.Models
+{
+    public partial class EmployeeInfo
+    {
+
+        private class EmployeeInfoMetdata
+        {
+            public int EmpNo { get; set; }
+            [Required]
+            public string EmpName { get; set; }
+            [Required]
+            public string DeptName { get; set; }
+            [Required]
+            public string Designation { get; set; }
+            [Required]
+            public decimal Salary { get; set; }
+        }
+        
+    }
+}
---
+++ NG_Validations/Scripts/MyScripts/Logic.js
@@ -0,0 +1,41 @@
+﻿var app;
+(function () {
+    app = angular.module('appmodule', []);
+})();
+
+
+//The Directive for Custom Validation for the Attribute Directive
+app.directive('requiredUniquevalue', function ($http) {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attribute, ctrl) {
+            scope.$watch(attribute.ngModel, function () {
+                $http({
+                    method: 'POST',
+                    url: '/api/EmployeeInfoAPI/' + attribute.requiredUniquevalue,
+                    data: { 'EmpNo': attribute.requiredUniquevalue }
+                }).success(function (res) {
+                    if (res) {
+                        ctrl.$setValidity('requiredUniquevalue', false);
+                    } else {
+                        ctrl.$setValidity('requiredUniquevalue', true);
+                    }
+                }).error(function (err) {
+                    ctrl.$setValidity('requiredUniquevalue', false);
+                });
+            });
+        }
+    }
+});
+
+//The COntroller
+app.controller('appctrl', function ($scope) {
+    $scope.Employee = {
+        EmpNo: 0,
+        EmpName: "",
+        DeptName: "",
+        Salary: "",
+        Designation: "",
+    };
+     
+});
